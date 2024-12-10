0833979b1d02c62e5788560def4036ac5e231cc5
livexia <istillalive@live.cn>
2024-12-10 13:57:43 +0800

    finish day 10

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::error::Error;
 use std::io::{self, Read};
 use std::time::Instant;
@@ -33,7 +33,7 @@ fn find_trailheads(map: &TopoMap) -> Vec<(usize, usize)> {
     trailheads
 }
 
-fn find_hiking_trail(coord: (usize, usize), map: &TopoMap) -> Vec<(usize, usize)> {
+fn dfs_find_hiking_trail(coord: (usize, usize), map: &TopoMap) -> Vec<(usize, usize)> {
     let (x, y) = coord;
     let height = map[x][y];
     if height == 9 {
@@ -41,16 +41,16 @@ fn find_hiking_trail(coord: (usize, usize), map: &TopoMap) -> Vec<(usize, usize)
     } else {
         let mut result = vec![];
         if x > 0 && map[x - 1][y] == height + 1 {
-            result.extend(find_hiking_trail((x - 1, y), map).iter())
+            result.extend(dfs_find_hiking_trail((x - 1, y), map).iter())
         }
         if x + 1 < map.len() && map[x + 1][y] == height + 1 {
-            result.extend(find_hiking_trail((x + 1, y), map).iter())
+            result.extend(dfs_find_hiking_trail((x + 1, y), map).iter())
         }
         if y > 0 && map[x][y - 1] == height + 1 {
-            result.extend(find_hiking_trail((x, y - 1), map).iter())
+            result.extend(dfs_find_hiking_trail((x, y - 1), map).iter())
         }
         if y + 1 < map[0].len() && map[x][y + 1] == height + 1 {
-            result.extend(find_hiking_trail((x, y + 1), map).iter())
+            result.extend(dfs_find_hiking_trail((x, y + 1), map).iter())
         }
         result
     }
@@ -63,20 +63,12 @@ fn part1(map: &TopoMap) -> Result<usize> {
     let result = trailheads
         .iter()
         .map(|&c| {
-            find_hiking_trail(c, map)
+            dfs_find_hiking_trail(c, map)
                 .iter()
                 .collect::<HashSet<_>>()
                 .len()
         })
         .sum();
-    println!(
-        "{}",
-        trailheads
-            .iter()
-            .map(|&c| find_hiking_trail(c, map).len())
-            .sum::<usize>()
-    );
-
     println!("part1: {result}");
     println!("> Time elapsed is: {:?}", _start.elapsed());
     Ok(result)
@@ -88,10 +80,66 @@ fn part2(map: &TopoMap) -> Result<usize> {
     let trailheads = find_trailheads(map);
     let result = trailheads
         .iter()
-        .map(|&c| find_hiking_trail(c, map).len())
+        .map(|&c| dfs_find_hiking_trail(c, map).len())
         .sum::<usize>();
 
     println!("part2: {result}");
+    println!("> Time elapsed is: {:?}", _start.elapsed());
+    Ok(result)
+}
+
+fn bfs_find_hiking_trail(coord: (usize, usize), map: &TopoMap, part2: bool) -> usize {
+    let mut queue = VecDeque::new();
+    queue.push_back(coord);
+    let mut visited = HashSet::new();
+    let mut result = 0;
+    while let Some(current) = queue.pop_front() {
+        if visited.insert(current) || part2 {
+            let (x, y) = current;
+            let height = map[x][y];
+            if height == 9 {
+                result += 1;
+            } else {
+                if x > 0 && map[x - 1][y] == height + 1 {
+                    queue.push_back((x - 1, y));
+                }
+                if x + 1 < map.len() && map[x + 1][y] == height + 1 {
+                    queue.push_back((x + 1, y));
+                }
+                if y > 0 && map[x][y - 1] == height + 1 {
+                    queue.push_back((x, y - 1));
+                }
+                if y + 1 < map.len() && map[x][y + 1] == height + 1 {
+                    queue.push_back((x, y + 1));
+                }
+            }
+        }
+    }
+    result
+}
+
+fn part1_bfs(map: &TopoMap) -> Result<usize> {
+    let _start = Instant::now();
+
+    let trailheads = find_trailheads(map);
+    let result = trailheads
+        .iter()
+        .map(|&c| bfs_find_hiking_trail(c, map, false))
+        .sum();
+    println!("part1 with bfs: {result}");
+    println!("> Time elapsed is: {:?}", _start.elapsed());
+    Ok(result)
+}
+
+fn part2_bfs(map: &TopoMap) -> Result<usize> {
+    let _start = Instant::now();
+
+    let trailheads = find_trailheads(map);
+    let result = trailheads
+        .iter()
+        .map(|&c| bfs_find_hiking_trail(c, map, true))
+        .sum();
+    println!("part2 with bfs: {result}");
     println!("> Time elapsed is: {:?}", _start.elapsed());
     Ok(result)
 }
@@ -103,6 +151,8 @@ fn main() -> Result<()> {
     let map = parse_input(input)?;
     part1(&map)?;
     part2(&map)?;
+    part1_bfs(&map)?;
+    part2_bfs(&map)?;
     Ok(())
 }
 
@@ -119,7 +169,9 @@ fn example_input() -> Result<()> {
 ";
     let map = parse_input(input)?;
     assert_eq!(part1(&map)?, 36);
+    assert_eq!(part1_bfs(&map)?, 36);
     assert_eq!(part2(&map)?, 81);
+    assert_eq!(part2_bfs(&map)?, 81);
     Ok(())
 }
 
@@ -128,7 +180,9 @@ fn real_input() -> Result<()> {
     let input = std::fs::read_to_string("input/input.txt").unwrap();
     let map = parse_input(input)?;
     assert_eq!(part1(&map)?, 566);
+    assert_eq!(part1_bfs(&map)?, 566);
     assert_eq!(part2(&map)?, 1324);
+    assert_eq!(part2_bfs(&map)?, 1324);
     assert_eq!(2, 2);
     Ok(())
 }
