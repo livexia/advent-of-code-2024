0d245bf7a7361f2dc268ab03a068b50553299126
livexia <istillalive@live.cn>
2024-12-12 15:47:33 +0800

    day 12: finish part 2

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::{self, Read};
 use std::time::Instant;
@@ -19,8 +19,8 @@ fn parse_input<T: AsRef<str>>(input: T) -> Result<Vec<Vec<char>>> {
         .collect())
 }
 
-fn dfs(x: usize, y: usize, map: &[Vec<char>], area: &mut HashSet<(usize, usize)>) -> usize {
-    if area.insert((x, y)) {
+fn dfs(x: usize, y: usize, map: &[Vec<char>], area: &mut HashSet<(isize, isize)>) -> usize {
+    if area.insert((x as isize, y as isize)) {
         let mut perimeter = 0;
         for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
             if (x == 0 && dx == -1)
@@ -49,10 +49,10 @@ fn part1(map: &[Vec<char>]) -> Result<usize> {
     let _start = Instant::now();
 
     let mut result = 0;
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
     for i in 0..map.len() {
         for j in 0..map[0].len() {
-            if !visited.contains(&(i, j)) {
+            if !visited.contains(&(i as isize, j as isize)) {
                 let mut area = HashSet::new();
                 let p = dfs(i, j, map, &mut area);
                 result += area.len() * p;
@@ -66,46 +66,56 @@ fn part1(map: &[Vec<char>]) -> Result<usize> {
     Ok(result)
 }
 
-fn number_of_side(x: usize, y: usize, area: &HashSet<(usize, usize)>) -> usize {
-    // (x, y) must in side, we can start from here,
-    // also (x - 1, y) and (x ,y - 1) aren't in the region
-    if area.contains(&(x + 1, y)) {
-        todo!()
-    } else if area.contains(&(x, y + 1)) {
-        todo!()
-    } else {
-        assert_eq!(area.len(), 1);
-        4
+fn number_of_side(area: &HashSet<(isize, isize)>) -> usize {
+    type Coord = (isize, isize);
+    let mut side: HashMap<(Coord, Coord), HashSet<Coord>> = HashMap::new();
+    for &(x, y) in area.iter() {
+        let next: Vec<_> = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            .into_iter()
+            .filter(|&(dx, dy)| !area.contains(&(x + dx, y + dy)))
+            .collect();
+        if next.is_empty() {
+            continue;
+        }
+        for (dx, dy) in next {
+            let line = (x * dx.abs(), dy * dy.abs());
+            side.entry((line, (dx, dy)))
+                .or_default()
+                .insert((x + dx, y + dy));
+        }
     }
-}
-
-fn search_side(
-    x: isize,
-    y: isize,
-    start: (isize, isize),
-    direction: (isize, isize),
-    area: &HashSet<(isize, isize)>,
-) -> usize {
-    if (x, y) == start {
-        1
-    } else {
-        // AA
-        // AAA
-        todo!()
+    let mut count = 0;
+    for points in side.values() {
+        let mut visited = HashSet::new();
+        for &(x, y) in points {
+            if visited.insert((x, y)) {
+                count += 1;
+                (1..)
+                    .map(|i| (x - i, y))
+                    .take_while(|c| points.contains(c))
+                    .chain((1..).map(|i| (x + i, y)).take_while(|c| points.contains(c)))
+                    .chain((1..).map(|i| (x, y - i)).take_while(|c| points.contains(c)))
+                    .chain((1..).map(|i| (x, y + i)).take_while(|c| points.contains(c)))
+                    .for_each(|c| {
+                        visited.insert(c);
+                    });
+            }
+        }
     }
+    count
 }
 
 fn part2(map: &[Vec<char>]) -> Result<usize> {
     let _start = Instant::now();
 
     let mut result = 0;
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
     for i in 0..map.len() {
         for j in 0..map[0].len() {
-            if !visited.contains(&(i, j)) {
+            if !visited.contains(&(i as isize, j as isize)) {
                 let mut area = HashSet::new();
                 let _ = dfs(i, j, map, &mut area);
-                result += number_of_side(i, j, &area) * area.len();
+                result += number_of_side(&area) * area.len();
                 visited.extend(&area);
             }
         }
@@ -134,7 +144,7 @@ BBCC
 EEEC";
     let map = parse_input(input)?;
     assert_eq!(part1(&map)?, 140);
-    assert_eq!(part2(&map)?, 436);
+    assert_eq!(part2(&map)?, 80);
     Ok(())
 }
 
@@ -147,7 +157,7 @@ OXOXO
 OOOOO";
     let map = parse_input(input)?;
     assert_eq!(part1(&map)?, 772);
-    assert_eq!(part2(&map)?, 368);
+    assert_eq!(part2(&map)?, 436);
     Ok(())
 }
 
@@ -170,8 +180,22 @@ MMMISSJEEE";
 }
 
 #[test]
+fn example_input3() -> Result<()> {
+    let input = "EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE";
+    let map = parse_input(input)?;
+    assert_eq!(part2(&map)?, 236);
+    Ok(())
+}
+
+#[test]
 fn real_input() -> Result<()> {
     let input = std::fs::read_to_string("input/input.txt").unwrap();
-    assert_eq!(2, 2);
+    let map = parse_input(input)?;
+    assert_eq!(part1(&map)?, 1494342);
+    assert_eq!(part2(&map)?, 893676);
     Ok(())
 }
